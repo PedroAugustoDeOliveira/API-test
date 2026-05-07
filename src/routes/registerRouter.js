3118c5e0b82f073a4b9a3501c0af41e445c4409f
PedroAugustoDeOliveira <pedroaugusto6467@gmail.com>
2026-05-07 11:34:17 -0300

    feat: adding a middleware to validate the register

--- src/routes/registerRouter.js
+++ src/routes/registerRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { postRegister } from "../controllers/resgisterController.js";
+import { validateRegister } from "../middlewares/validateRegister.js";
 
 const resgiterRouter = Router();
 
-resgiterRouter.post("/register", postRegister);
+resgiterRouter.post("/register", validateRegister, postRegister);
 
 export default resgiterRouter;
